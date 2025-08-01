bceb221d257da0a67fcdda347f80adbddf4882f9
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-08-02 00:19:04 +0530

    Initial commit

---
+++ lec3.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class lec3{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number :- ");
+        int a = sc.nextInt();
+        System.out.println("Enter second number :- ");
+        int b = sc.nextInt();
+        System.out.println("Final result is :-");
+        int sum = a+b;
+        System.err.println(sum);
+
+    }
+
+    
+}
