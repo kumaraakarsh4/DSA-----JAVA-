a0c87e823e2f2069af54858e247a40394e72d65a
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-08-14 01:12:06 +0530

    Added
    Adding my dsa problem day 13

--- lec13.java
+++ lec13.java
@@ -1,62 +1,62 @@
-// public class lec13 {
-//     public static void arrayPair(int numbers[]){
-//         for(int i = 0; i< numbers.length;i++){
-//             int curr = numbers[i];
-//             for(int j = i+1; j<numbers.length;j++){
-//                 System.out.print("(" + curr + " ," + numbers[j] + ")");
-//             }
-//             System.out.println();
-//         }
-//     }
-//     public static void main(String[] args) {
-//         int numbers[]= {2,4,6,8,10};
-//         arrayPair(numbers);
-//     }
+public class lec13 {
+    public static void arrayPair(int numbers[]){
+        for(int i = 0; i< numbers.length;i++){
+            int curr = numbers[i];
+            for(int j = i+1; j<numbers.length;j++){
+                System.out.print("(" + curr + " ," + numbers[j] + ")");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int numbers[]= {2,4,6,8,10};
+        arrayPair(numbers);
+    }
     
-// }
+}
 
-// practice pairs of array
-// public class lec13 {
-//     public static void pair(int numbers[]){
-//         for(int i = 0; i<numbers.length; i++){
-//             int curr = numbers[i];
-//             for(int j = i+1; j<numbers.length;j++){
-//                 System.out.print("(" + curr + "," + numbers[j] + " )");
-//             }
-//             System.out.println();
-//         }
-//     }
+practice pairs of array
+public class lec13 {
+    public static void pair(int numbers[]){
+        for(int i = 0; i<numbers.length; i++){
+            int curr = numbers[i];
+            for(int j = i+1; j<numbers.length;j++){
+                System.out.print("(" + curr + "," + numbers[j] + " )");
+            }
+            System.out.println();
+        }
+    }
 
-//     public static void main(String[] args) {
-//         int numbers[]={1,3,4,7,9};
-//         pair(numbers);
-//     }
-// }
+    public static void main(String[] args) {
+        int numbers[]={1,3,4,7,9};
+        pair(numbers);
+    }
+}
 
-// pratice pairs array --->
+pratice pairs array --->
 
-// public class lec13 {
-//     public static void paired(int numbers[]){
-//         int tp = 0;
-//         for(int i = 0; i<numbers.length;i++){
-//             int curr = numbers[i];
-//             for(int j = i+1; j<numbers.length;j++){
-//                 System.out.print("(" + curr + "," + numbers[j]+ ")");
+public class lec13 {
+    public static void paired(int numbers[]){
+        int tp = 0;
+        for(int i = 0; i<numbers.length;i++){
+            int curr = numbers[i];
+            for(int j = i+1; j<numbers.length;j++){
+                System.out.print("(" + curr + "," + numbers[j]+ ")");
 
-//                 tp++;
-//             }
-//             System.out.println();
-//         }
-//         System.out.println("Total pairs : -  " + tp);
-//     }
+                tp++;
+            }
+            System.out.println();
+        }
+        System.out.println("Total pairs : -  " + tp);
+    }
 
-//     public static void main(String[] args) {
-//         int numbers[]={1,2,3,4,5,6};
-//         paired(numbers);
-//     }
-// }
+    public static void main(String[] args) {
+        int numbers[]={1,2,3,4,5,6};
+        paired(numbers);
+    }
+}
 
-// practice question ----> pairs array
+practice question ----> pairs array
 
 public class lec13 {
     public static void prind(int numbers[]){
