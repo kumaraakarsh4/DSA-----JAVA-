676cf450ff934c008a404383625f37b19360664b
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-08-17 22:03:22 +0530

    Added Me
    questions solved Selection sort

--- selectionsort.java
+++ selectionsort.java
@@ -1,28 +1,89 @@
-public class selectionsort {
-    public static void selectionSort(int arr[]){
-        for(int i = 0;i<arr.length-1;i++){
-            int min = i;
-            for(int j = i+1 ; j<arr.length;j++){
-                if (arr[min] > arr[j]) {
-                    min = j;
+// public class selectionsort {
+//     public static void selectionSort(int arr[]){
+//         for(int i = 0;i<arr.length-1;i++){
+//             int min = i;
+//             for(int j = i+1 ; j<arr.length;j++){
+//                 if (arr[min] > arr[j]) {
+//                     min = j;
                     
-                }
-            }
-            int temp = arr[min];
-            arr[min]= arr[i];
-            arr[i]= temp;
-        }
-    }
-    public static void printArr(int arr[]){
-        for(int i = 0; i<arr.length;i++){
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-    }
-    public static void main(String[] args) {
-        int arr[]={5,4,3,2,1};
-        selectionSort(arr);
-        printArr(arr);
-    }
+//                 }
+//             }
+//             int temp = arr[min];
+//             arr[min]= arr[i];
+//             arr[i]= temp;
+//         }
+//     }
+//     public static void printArr(int arr[]){
+//         for(int i = 0; i<arr.length;i++){
+//             System.out.print(arr[i] + " ");
+//         }
+//         System.out.println();
+//     }
+//     public static void main(String[] args) {
+//         int arr[]={5,4,3,2,1};
+//         selectionSort(arr);
+//         printArr(arr);
+//     }
     
-}
+// }
+
+// praticing selection sort questions
+
+// public class selectionsort {
+//     public static void selectionSo(int arr[]){
+//         for(int i = 0; i<arr.length-1;i++){
+//             int min = i;
+//             for(int j = i+1;j<arr.length;j++){
+//                 if (arr[min] > arr[j]) {
+//                     min = j;
+                    
+//                 }
+//             }
+//             int temp = arr[min];
+//             arr[min] = arr[i];
+//             arr[i] = temp;
+//         }
+//     }
+//     public static void prinArr(int arr[]){
+//         for(int i =0; i<arr.length;i++){
+//             System.out.print(arr[i] + " ");
+//         }
+//         System.out.println();
+//     }
+//     public static void main(String[] args) {
+//         int arr[]={10,8,6,4,2};
+//         selectionSo(arr);
+//         prinArr(arr);
+//     }
+// }
+
+// next question pratice
+
+// public class selectionsort {
+//     public static void selectSort(int arr[]){
+//         for(int i =0; i<arr.length-1;i++){
+//             int min = i;
+//             for(int j = i+1;j<arr.length;j++){
+//                 if (arr[min] > arr[j]) {
+//                     min = j;
+                    
+//                 }
+//             }
+//             int temp = arr[min];
+//             arr[min]= arr[i];
+//             arr[i] = temp;
+//         }
+//     }
+//     public static void priArr(int arr[]){
+//         for(int i =0; i<arr.length;i++){
+//             System.out.print(arr[i] + " ");
+//         }
+//         System.out.println();
+//     }
+
+//     public static void main(String[] args) {
+//         int arr[] = {11,9,7,5,3,1};
+//         selectSort(arr);
+//         priArr(arr);
+//     }
+// }
