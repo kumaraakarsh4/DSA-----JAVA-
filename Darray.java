54b2d9e48bd21c6caeb76d2641b023f2ac0df583
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-08-18 23:45:35 +0530

    Added Me
    Today i have solved 2d array question

---
+++ Darray.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class Darray {
+    public static boolean search(int matrix [][] , int key){
+        for(int i =0;i<matrix.length;i++){
+            for(int j =0; j<matrix[0].length;j++){
+                if (matrix[i][j] == key) {
+                    System.out.print("The key is at (" + i + "," + j + ")");
+                    return true;
+                    
+                }
+            }
+            
+          }
+          System.out.println("Key not found");
+          return false;
+
+
+    }
+    public static void main(String[] args) {
+        int matrix [] [] = new int [3] [3];
+      
+        int n = matrix.length , m = matrix[0].length;
+          Scanner sc = new Scanner(System.in);
+          for(int i =0; i<n;i++){
+            for(int j =0; j<m;j++){
+                matrix[i][j] = sc.nextInt();
+            }
+          }
+          for(int i =0;i<n;i++){
+            for(int j =0; j<n;j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+             System.out.println();
+          }
+          search(matrix, 8);
+         
+
+        
+    }
+    
+}
