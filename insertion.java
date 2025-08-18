d69779611721e551f49673eef4651eb3e979a47d
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-08-18 15:51:48 +0530

    Added Me
    I am adding new question of insertion sort

--- insertion.java
+++ insertion.java
@@ -113,3 +113,31 @@ public class insertion {
         PrinArrays(arr);
     }
 }
+
+praticing new question insertion sort
+public class insertion {
+    public static void inerrSort(int arr[]){
+        for(int i = 0; i<arr.length;i++){
+            int curr = arr[i];
+            int prev = i-1;
+            while (prev >= 0 && arr[prev] > curr) {
+                arr[prev+1] = arr[prev];
+                prev--;
+                
+            }
+            arr[prev+1] = curr;
+        }
+    }
+    public static void prinner(int arr[]){
+        for(int i = 0; i<arr.length;i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {66,87,47,31,20,10,15};
+        inerrSort(arr);
+        prinner(arr);
+    }
+}
