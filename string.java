2473aff9113b9079fce1f2d5cc38e2a441647fc7
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-08-19 23:53:06 +0530

    Added Me
    i have also solved shortest path question

--- string.java
+++ string.java
@@ -75,3 +75,67 @@ public class string {
         System.out.println(isPaland(str));
     }
 }
+
+new string question
+
+public class string {
+    public static float getPaths(String path){
+        int x =0 , y=0;
+        for(int i=0;i<path.length();i++){
+            char dir = path.charAt(i);
+            if (dir == 'N') {
+                y++;
+                
+            }
+            else if (dir == 'S') {
+                y--;
+                
+            }
+            else if (dir == 'W') {
+                x--;
+                
+            }
+            else{
+                x++;
+            }
+        }
+        int X2 = x*x;
+        int Y2 = y*y;
+        return (float)Math.sqrt(X2 + Y2);
+    }
+
+    public static void main(String[] args) {
+        String path = "WNEENESENNN";
+        System.out.println(getPaths(path));
+    }
+}
+
+doing the same question shotest path
+
+public class string {
+    public static float shortPath(String path){
+        int x =0 , y=0;
+        for(int i =0; i<path.length();i++){
+            char dir = path.charAt(i);
+            if (dir == 'N') {
+               y++; 
+            }else if (dir == 'S') {
+                y--;
+                
+            }else if (dir == 'w') {
+                x--;
+                
+            }else{
+                x++;
+            }
+        }
+        int X2 = x*x;
+        int Y2 = y*y;
+        return (float)Math.sqrt(X2 + Y2);
+    }
+
+    public static void main(String[] args) {
+        String path = "WN";
+        System.out.println(shortPath(path));
+    }
+}
