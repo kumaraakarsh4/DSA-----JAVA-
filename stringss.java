d10ee8c163ec8465c0aa8fdad76b498383e903e1
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-08-20 01:45:18 +0530

    Added Me
     i have solved the string dsa question

--- stringss.java
+++ stringss.java
@@ -30,3 +30,44 @@ public class stringss {
         System.out.println(stris(str, 0, 7));
     }
 }
+
+finding largest string 
+
+public class stringss {
+
+    public static void main(String[] args) {
+        String fruit [] ={"apple" , "mango" , "banana"};
+        String largest = fruit[0];
+        for(int i=0; i<fruit.length;i++){
+           
+            if ( largest.compareTo(fruit[i]) < 0 ) {
+                largest = fruit[i];
+                
+            }
+        }
+        System.out.println(largest);
+    }
+}
+
+question of find vowels in an string
+
+import java.util.*;
+public class stringss {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the name :-");
+        String str = sc.nextLine();
+        int count = 0;
+        for(int i=0;i<str.length();i++){
+            char ch = str.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                count++;
+                
+            }
+
+        }
+        System.out.println("Count of vowels is :-- " + count);
+        
+    }
+}
