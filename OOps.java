f187f8139a616a377e959e9aff05204cc457e1a4
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-08-25 01:23:50 +0530

    Added Me
    today i have oops new types polymorphism

--- OOps.java
+++ OOps.java
@@ -130,3 +130,28 @@ class parrot extends bird{
         System.out.println("can speaking");
     }
 }
+
+polymorphism -----
+
+public class OOps {
+
+    public static void main(String[] args) {
+        calculator calc = new calculator();
+        System.out.println(calc.sum(5, 2));
+        System.out.println(calc.sum((float)1.5, (float)2.2));
+        System.out.println(calc.sum(6, 02, 02));
+        
+    }
+}
+class calculator{
+    int sum(int a, int b){
+        return a+b;
+    }
+    float sum(float a , float b){
+        return a + b ;
+    }
+    int sum(int a , int b , int c){
+        return a + b + c;
+    }
+
+}
