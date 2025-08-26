3a23d5058fb6e8efed86643a9681ec111419c10a
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-08-27 01:30:00 +0530

    Added Me
    Today i am updating Recurssion new questions

--- Recurrsion.java
+++ Recurrsion.java
@@ -76,4 +76,83 @@ public class Recurrsion {
         int n = 5;
         System.out.println("The final answer is :- " + Fact(n));
     }
+} 
+
+question of recursion calculate factorial
+
+public class Recurrsion {
+    public static int Fact(int n ){
+        if (n == 0) {
+            return 1;
+            
+        }
+        int fm = Fact(n-1);
+        int fn = n*Fact(n-1);
+        return fn;
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println("the value is :- " + Fact(n));
+    }
+}
+
+next question print sum of n natural number
+
+public class Recurrsion {
+    public static int calcSum(int n){
+        if (n == 1) {
+            return 1;
+            
+        }
+       int  snm1 = calcSum(n-1);
+       int sn = n + snm1;
+       return sn;
+
+    }
+
+    public static void main(String[] args) {
+        int n =5;
+        System.out.println(calcSum(n));
+    }
+}
+
+printing the fibonaci series
+
+public class Recurrsion {
+    public static int fibonaci(int n ){
+        if (n == 0 || n == 1) {
+            return n;
+            
+        }
+        int fnm = fibonaci(n-1);
+        int fn = fibonaci(n-2);
+        int sum = fnm + fn ; 
+        return sum ;
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println("The sum of number is :- " + fibonaci(n));
+    }
+}
+
+question of fibonaci series
+
+public class Recurrsion {
+    public static int finnac(int n){
+        if (n == 0 || n == 1) {
+            return n;
+            
+        }
+        int fnm = finnac(n-1);
+        int fn = finnac(n-2);
+        int sum = fnm + fn;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int n = 11;
+        System.out.println("The sum of number is :-- " + finnac(n));
+    }
 }
