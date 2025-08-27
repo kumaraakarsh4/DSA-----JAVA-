e3e237e2bbb4016db7b798b5bc4956ec5f29ab84
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-08-28 01:16:11 +0530

    Added Me
    Today i have solved a new recursive question

--- Recurrsion.java
+++ Recurrsion.java
@@ -156,3 +156,25 @@ public class Recurrsion {
         System.out.println("The sum of number is :-- " + finnac(n));
     }
 }
+
+next recurssion question check sorted array
+
+public class Recurrsion {
+    public static boolean isSorted(int arr[] , int i){
+        if (i == arr.length-1) {
+            return true;
+            
+        }
+        if (arr[i] > arr[i+1]) {
+            return false;
+            
+        }
+        return isSorted(arr, i+1);
+    }
+
+    public static void main(String[] args) {
+        int arr[] ={1,2,3,4,5};
+       System.out.println( isSorted(arr, 0));
+        
+    }
+}
