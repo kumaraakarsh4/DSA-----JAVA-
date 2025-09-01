a332ef1cef3ef2ceade9791a77080dcfbb7c134b
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-09-02 02:11:00 +0530

    Added Me
    today i have solved recurssion new question removing duplicates string

--- Recurrsion.java
+++ Recurrsion.java
@@ -376,3 +376,30 @@ public class Recurrsion {
     
   }
 }
+
+NEXT RECURSSION QUESTION REMOVING DUPLICATES FROM STRING
+
+public class Recurrsion {
+  public static void duplicate(String str , int indx , StringBuilder newStr , boolean map[]){
+    if (indx == str.length()) {
+      System.out.println(newStr);
+      return;
+      
+    }
+    char currChar = str.charAt(indx);
+    if (map[currChar -'a'] == true) {
+      duplicate(str, indx+1, newStr, map);
+      
+    }else{
+      map[currChar-'a'] = true;
+      duplicate(str, indx+1, newStr.append(currChar), map);
+    }
+
+  }
+
+  public static void main(String[] args) {
+    String str = "aakarshhkumar";
+    duplicate(str, 0,new StringBuilder(""), new boolean[26]);
+    
+  }
+}
