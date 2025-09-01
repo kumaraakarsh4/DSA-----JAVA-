31c65f0b29c725a2d43ab52b821a3bd58325d1cd
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-09-02 00:47:25 +0530

    Added Me
    today i have solved recurssion tiles problem

--- Recurrsion.java
+++ Recurrsion.java
@@ -354,3 +354,25 @@ public class Recurrsion {
     
   }
 }
+
+
+recurssion tailing problem
+
+public class Recurrsion {
+  public static int tailing(int n){
+    if (n == 0 || n == 1) {
+      return 1;
+      
+    }
+
+    int fnm1 = tailing(n-1);
+    int fnm2 = tailing(n-2);
+    int totalway = fnm1 + fnm2;
+    return totalway;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("The total way are :-- " + tailing(4));
+    
+  }
+}
