0d0c9802e1a7446cb324eb8be336676a3e4122cc
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-09-04 01:45:15 +0530

    Added Me
    I am adding divide and conquer quick sort question

--- Divideandconquer.java
+++ Divideandconquer.java
@@ -1,4 +1,4 @@
-// DIVIDE AND CONQUER [MERGE SORT]
+DIVIDE AND CONQUER [MERGE SORT]
 public class Divideandconquer {
     public static void printArr(int arr[]){
         for(int i=0;i<arr.length;i++){
@@ -57,4 +57,51 @@ public class Divideandconquer {
         
     }
     
+}
+
+// DIVIDE AND CONQUER [QUICK SORT]
+public class Divideandconquer {
+    public static void printArr(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void quickSort(int arr[] , int si , int ei){
+        if (si >= ei) {
+            return;
+            
+        }
+        int pidx = partition(arr , si , ei);
+        quickSort(arr, si, pidx-1); // for left part
+        quickSort(arr, pidx+1, ei); // for right part
+    }
+    public static int partition(int arr[] ,   int si, int ei){
+        int pivot = arr[ei];
+        int i = si -1 ; // for left iterator
+        for(int j=si; j<ei;j++){
+            if (arr[j] <= pivot) {
+                i++;
+                // we swap the element
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+                
+            }
+        }
+        i++;
+        // we swap the element for pivot
+        int temp = pivot;
+        arr[ei] = arr[i];
+        arr[i] = temp;
+        return i;
+    }
+        
+
+    public static void main(String[] args) {
+        int arr[] = {6,3,9,5,2,8};
+        quickSort(arr, 0, arr.length-1);
+        printArr(arr);
+
+    }
 }
