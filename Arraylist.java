aec12e13f96b126d692f95779539163773acec23
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-09-08 02:13:18 +0530

    Added Me
    I adding my array list question

--- Arraylist.java
+++ Arraylist.java
@@ -17,3 +17,24 @@ public class Arraylist {
     }
     
 }
+
+PRINTING THE LIST OF ARRAY
+
+import java.util.ArrayList;
+
+public class Arraylist {
+
+    public static void main(String[] args) {
+        ArrayList<Integer>list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        for(int i=0;i<list.size();i++){
+            System.out.print(list.get(i) + " ");
+        }
+        System.out.println();
+    }
+}
+
