cd2465804f2e820f3fe9e18a093b1389a78ef363
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-09-09 00:02:03 +0530

    Added Me
    Today i solved Arraylist new question

--- Arraylist.java
+++ Arraylist.java
@@ -38,3 +38,29 @@ public class Arraylist {
     }
 }
 
+
+FIND THE MAXIMUM IN AN ARRAY LIST
+
+import java.util.ArrayList;
+
+public class Arraylist {
+
+    public static void main(String[] args) {
+        ArrayList<Integer>list = new ArrayList<>();
+        list.add(2);
+        list.add(5);
+        list.add(9);
+        list.add(3);
+        list.add(6);
+
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<list.size();i++){
+            if (max < list.get(i)) {
+                max = list.get(i);
+                
+            }
+        }
+        System.out.println("Maximum element is = " + max);
+
+    }
+}
