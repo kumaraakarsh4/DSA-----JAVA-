0690c090172b6e8646e98f6f7139249d0f57eba3
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-09-09 00:32:22 +0530

    Added Me
    I am updating my Arraylist  sorting question

--- Arraylist.java
+++ Arraylist.java
@@ -92,3 +92,45 @@ public class Arraylist {
 
     }
 }
+
+SORTIN AN ARRAY LIST ASCENDING ORDER
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Arraylist {
+
+    public static void main(String[] args) {
+        ArrayList<Integer>list = new ArrayList<>();
+        list.add(2);
+        list.add(5);
+        list.add(9);
+        list.add(3);
+        list.add(6);
+        System.out.println(list);
+       Collections.sort(list);
+        System.out.println(list);
+    }
+}
+
+ SORTING AN ARRAY LIST IN DECENDING ORDER
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+public class Arraylist {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(5);
+        list.add(9);
+        list.add(3);
+        list.add(6);
+        System.out.println(list);
+
+        Collections.sort(list , Collections.reverseOrder());
+        System.out.println(list);
+    }
+}
