c0899919dff344af992ad9683bf09edfc48e1140
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-09-09 02:12:32 +0530

    Added Me
    I have solved arraylist most water question

--- Arraylist.java
+++ Arraylist.java
@@ -134,3 +134,37 @@ public class Arraylist {
         System.out.println(list);
     }
 }
+
+ SOLVING QUESTION OF CONTAINER WITH MOST WATER
+
+import java.util.ArrayList;
+
+public class Arraylist {
+    public static int mostWater(ArrayList<Integer> height){
+        int maxWater =0;
+        for(int i=0;i<height.size();i++){
+            for(int j=i+1;j<height.size();j++){
+                int ht = Math.min(height.get(i), height.get(j));
+                int width = j-i;
+                int currwater = ht * width;
+                maxWater = Math.max(maxWater, currwater);
+            }
+        }
+        return maxWater;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> height = new ArrayList<>();
+        height.add(1);
+        height.add(8);
+         height.add(6);
+          height.add(2);
+           height.add(5);
+            height.add(4);
+             height.add(8);
+        height.add(3);
+        height.add(7);
+
+        System.out.println(mostWater(height));
+    }
+}
