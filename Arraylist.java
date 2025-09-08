67366adf080d07ae44dd111ed4de633e8dfe6563
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-09-09 02:33:15 +0530

    Added Me
    I have solved pair sum question of array list

--- Arraylist.java
+++ Arraylist.java
@@ -168,3 +168,35 @@ public class Arraylist {
         System.out.println(mostWater(height));
     }
 }
+
+ NEXT QUESTION FINDING PAIR SUM OF ARRAY
+
+import java.util.ArrayList;
+
+public class Arraylist {
+    public static boolean paiSum(ArrayList<Integer> list , int target ){
+        for(int i=0;i<list.size();i++){
+            for(int j=i+1;j<list.size();j++){
+                if (list.get(i) + list.get(j) == target) {
+                    return true;
+                    
+                }
+            }
+        }
+        return false;
+
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        int target =50;
+        System.out.println(paiSum(list, target));
+
+    }
+}
