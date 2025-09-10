404a706a860f586ea57aeca19d90c2faca5aa2d5
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-09-11 00:16:32 +0530

    Added Me
    I am updating linkedlist question searching for a key

--- linkedcustom.java
+++ linkedcustom.java
@@ -412,3 +412,102 @@ public class linkedcustom {
     }
 }
 
+QUESTION OF LINKEDLIST SEARCH FOR A KEY
+
+import java.util.LinkedList;
+import java.util.*;
+
+public class linkedcustom {
+
+    public static class Node{
+        int data ;
+        Node next;
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void addFirst(int data){
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+            
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+            
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+    public void print(){
+        if (head == null) {
+            System.out.println("LL is empty");
+            
+            return;
+            
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "  -> ");
+            temp = temp.next;
+            
+        }
+        System.out.println("null");
+    }
+   public int search(int key){
+    Node temp = head;
+    int i =0;
+    while (temp != null) {
+        if (temp.data == key) {
+            return i;
+
+            
+        }
+         temp = temp.next;
+        i++;
+        
+    }
+    return -1;
+   }
+    
+    
+    public static void main(String[] args) {
+        linkedcustom ll = new linkedcustom();
+        
+          
+        
+        ll.addFirst(2);
+        
+        ll.addFirst(1);
+        
+        ll.addLast(3);
+        
+        ll.addLast(4);
+        // ll.add(1, 9);
+        
+        ll.print();
+    
+        System.out.println("key is found at :- " + ll.search(4));
+        System.out.println(ll.search(5));
+        System.out.println(ll.size);
+
+      
+
+    }
+}
+
