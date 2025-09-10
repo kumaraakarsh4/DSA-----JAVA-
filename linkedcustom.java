d33790485b32d4c1923aaee90d4de3d62e613ce2
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-09-11 00:53:32 +0530

    Added Me
    I am updating my linkedlist question reverse an linkedlist

--- linkedcustom.java
+++ linkedcustom.java
@@ -511,3 +511,99 @@ public class linkedcustom {
     }
 }
 
+NEXT QUESTION REVERSE LINKEDLIST
+
+import java.util.LinkedList;
+import java.util.*;
+
+public class linkedcustom {
+
+    public static class Node{
+        int data ;
+        Node next;
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void addFirst(int data){
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+            
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+            
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+    public void print(){
+        if (head == null) {
+            System.out.println("LL is empty");
+            
+            return;
+            
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "  -> ");
+            temp = temp.next;
+            
+        }
+        System.out.println("null");
+    }
+    public void reverse(){
+        Node prev =  null;
+        Node curr = tail = head;
+        Node next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+
+            
+        }
+        head = prev;
+    }
+    
+   
+        public static void main(String[] args) {
+        linkedcustom ll = new linkedcustom();
+        
+          
+        
+        ll.addFirst(2);
+        
+        ll.addFirst(1);
+        
+        ll.addLast(3);
+        
+        ll.addLast(4);
+        // ll.add(1, 9);
+        
+        ll.print();
+        ll.reverse();
+        ll.print();
+        
+      
+
+    }
+}
+
