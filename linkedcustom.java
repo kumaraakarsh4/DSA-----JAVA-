816236db1a0c48a642e1deedcc5b14648441253c
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-09-12 01:03:53 +0530

    Added Me
    I am updating question Flyoid cycle algorithm in linkedlist

--- linkedcustom.java
+++ linkedcustom.java
@@ -704,3 +704,46 @@ public class linkedcustom {
         
     }
 }
+
+FYOID CYCLE ALGORTITHM LINKEDLIST
+
+public class linkedcustom {
+      public static class Node{
+        int data ;
+        Node next;
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    public static boolean isCycle(){
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+                
+            }
+            
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+         linkedcustom ll = new linkedcustom();
+        
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        // head.next.next.next = head;
+        System.out.println(isCycle());
+         
+        
+    }
+}
