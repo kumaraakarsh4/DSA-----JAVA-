677702f1fe796d991da11c07fa74e697c17bf864
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-09-12 00:34:42 +0530

    Added Me
    I am updating linkedlist question checking number is palindrome or not

--- linkedcustom.java
+++ linkedcustom.java
@@ -607,3 +607,100 @@ public class linkedcustom {
     }
 }
 
+CHECK LINKEDLIST IS PALINDROME OR NOT 
+
+import java.util.LinkedList;
+import java.util.*;
+
+public class linkedcustom {
+
+    public static class Node{
+        int data ;
+        Node next;
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+   
+    
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+            
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+    public void print(){
+        if (head == null) {
+            System.out.println("LL is empty");
+            
+            return;
+            
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "  -> ");
+            temp = temp.next;
+            
+        }
+        System.out.println("null");
+    }
+    public Node fidMid(Node head){
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            
+        }
+        return slow;
+    }
+    public boolean isPalindrome(){
+        if (head == null || head.next == null) {
+            return true;
+            
+        }
+        Node miNode = fidMid(head);
+        Node prev = null;
+        Node curr = miNode;
+        Node next;
+        while (curr != null) {
+            next = curr.next;
+            prev = curr;
+            curr = next;
+            
+        }
+        Node right = prev;
+        Node left = head;
+        while (right != null) {
+            if (left.data != right.data) {
+                return false;
+                
+            }
+            left = left.next;
+            right = right.next;
+            
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+         linkedcustom ll = new linkedcustom();
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(2);
+        // ll.addLast(1);
+        ll.print();
+        System.out.println(ll.isPalindrome());
+        
+    }
+}
