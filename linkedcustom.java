18d367765d70a8b1db4416c3cd55c212409bd8a5
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-09-13 02:48:55 +0530

    Added Me
    i am updating linkedlist question printing in Zigzag

--- linkedcustom.java
+++ linkedcustom.java
@@ -747,3 +747,101 @@ public class linkedcustom {
         
     }
 }
+
+ PRINTING THE LINKEDLIST IN ZIGZAG
+
+import java.util.LinkedList;
+import java.util.*;
+
+public class linkedcustom {
+
+    public static class Node{
+        int data ;
+        Node next;
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+   
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+            
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+    public void print(){
+        if (head == null) {
+            System.out.println("LL is empty");
+            
+            return;
+            
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "  -> ");
+            temp = temp.next;
+            
+        }
+        System.out.println("null");
+    }
+   public void zigZag(){
+    Node slow = head;
+    Node fast = head;
+    while ( fast != null && fast.next != null) {
+        slow =  slow.next;
+        fast = fast.next.next;
+        
+    }
+    Node mid = slow;
+    Node curr = mid.next;
+    mid.next = null;
+    Node prev = null;
+    Node next;
+    while (curr != null) {
+        next = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+        
+    }
+    Node left = head;
+     Node right = prev;
+     Node nextl , nextR;
+     while (left != null && right != null) {
+        nextl = left.next;
+        left.next = right;
+        nextR = right.next;
+        right.next = nextl;
+        left = nextl;
+        right = nextR;
+
+        
+     }
+
+   }
+    public static void main(String[] args) {
+          linkedcustom ll = new linkedcustom();
+          ll.addLast(1);
+          ll.addLast(2);
+          ll.addLast(3);
+          ll.addLast(4);
+          ll.addLast(5);
+          ll.addLast(6);
+         ll.print();
+         ll.zigZag();
+         ll.print();
+
+        
+    }
+}
