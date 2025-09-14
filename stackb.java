be06c741af89255615d71251cce76c027d334e96
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-09-14 14:36:37 +0530

    Added Me
    I am updating stack question reverse a string using a stack (amazon , microsoft, adobe)

--- stackb.java
+++ stackb.java
@@ -143,3 +143,33 @@ public class stackb {
         }
     }
 }
+
+REVERSE A STRING USING A STACK
+
+import java.util.*;
+public class stackb {
+    public static String reverseString(String str){
+        Stack<Character> s = new Stack<>();
+        int indx = 0;
+        while (indx < str.length()) {
+            s.push(str.charAt(indx));
+            indx++;
+            
+}
+       StringBuilder result = new StringBuilder("");
+       while (!s.isEmpty()) {
+       char curr = s.pop();
+       result.append(curr);
+        
+       }
+       return result.toString();
+
+    }
+
+    public static void main(String[] args) {
+        String str = "abc";
+        String result = reverseString(str);
+        System.out.println(result);
+       
+    }
+}
