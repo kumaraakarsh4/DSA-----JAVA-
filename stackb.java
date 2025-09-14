3fcf524516a5c084aaeb12fcf52b9071c4f99398
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-09-14 18:28:32 +0530

    Added Me
    I am updating stack new question removing duplicate parenthesis [microsoft]

--- stackb.java
+++ stackb.java
@@ -366,3 +366,38 @@ public class stackb {
         System.out.println(isValid(str));
     }
 }
+
+REMOVING DUPLICATE PARENTHESIS 
+
+import java.util.*;
+public class stackb {
+public static boolean isDuplicate(String str){
+    Stack<Character> s = new Stack<>();
+    for(int i=0;i<str.length();i++){
+        char ch = str.charAt(i);
+        if (ch == ')') {
+            int count =0;
+            while (s.peek() != '(') {
+                s.pop();
+                count++;
+             }
+             if (count < 1) {
+                return true;
+                
+             }else{
+                s.pop();
+             }
+            }else{
+                s.push(ch);
+             }
+            
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        String str = "((a+b))"; // it return true
+        String str2 = "(a+b)"; // it return false
+        System.out.println(isDuplicate(str2));
+
+    }
+}
