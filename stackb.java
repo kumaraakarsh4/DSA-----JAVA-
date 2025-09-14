eeed7350717234f54d0517a971999da44ba072ed
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-09-14 13:59:42 +0530

    Added Me
    I am updating Stack amazon question push the stack at the bottom

--- stackb.java
+++ stackb.java
@@ -113,3 +113,33 @@ public class stackb {
         }
     }
 }
+
+PUSH AT THE BOTTOM OF THE STACK
+
+import java.util.*;
+public class stackb {
+    public static void pushAtBottom(Stack<Integer> s , int data){
+        if (s.isEmpty()) {
+            s.push(data);
+            return;
+            
+        }
+        int top = s.pop();
+        pushAtBottom(s, data);
+        s.push(top);
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        pushAtBottom(s , 4);
+
+        while (!s.isEmpty()) {
+            System.out.println(s.pop());
+            
+        }
+    }
+}
