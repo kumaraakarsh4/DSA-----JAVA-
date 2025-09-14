23a1e756c7f207bdca0c4202dff83bf0501ca6b6
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-09-14 17:28:42 +0530

    Added Me
    I am updating the stack question
    checking the parenthesis is valid or not

--- stackb.java
+++ stackb.java
@@ -284,3 +284,85 @@ public class stackb {
 }
      
 }
+
+CHECKING VALID PARENTHESIS EXIST OR NOT QUESTION
+
+import java.util.*;
+public class stackb {
+    public static boolean isValid(String str){
+        Stack<Character> s = new Stack<>();
+        for(int i=0;i<str.length();i++){
+            char ch = str.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {  // for opening bracket
+                s.push(ch);
+                
+            }else{  // for closing bracket
+                if (s.isEmpty()) {
+                    return false;
+                    
+                }
+                if ((s.peek() == '(' && ch == ')')|| (s.peek() == '{' && ch == '}') || (s.peek() == '[' && ch==']')){
+                s.pop();
+                    
+                }else{
+                    return false;
+                
+            
+            }
+        }
+    }
+    if (s.isEmpty()) {
+        return true;
+        
+    }else{
+        return false;
+    }
+    }
+    
+
+    public static void main(String[] args) {
+        String str = "({}[])";  // it should return true
+        System.out.println(isValid(str));
+    }
+}
+
+NEXT QUESTION OF VALID PARENTHESIS
+
+import java.util.*;
+public class stackb {
+    public static boolean isValid(String str){
+        Stack<Character> s = new Stack<>();
+        for(int i=0;i<str.length();i++){
+            char ch = str.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {  // for opening bracket
+                s.push(ch);
+                
+            }else{  // for closing bracket
+                if (s.isEmpty()) {
+                    return false;
+                    
+                }
+                if ((s.peek() == '(' && ch == ')')|| (s.peek() == '{' && ch == '}') || (s.peek() == '[' && ch==']')){
+                s.pop();
+                    
+                }else{
+                    return false;
+                
+            
+            }
+        }
+    }
+    if (s.isEmpty()) {
+        return true;
+        
+    }else{
+        return false;
+    }
+    }
+    
+
+    public static void main(String[] args) {
+        String str = "{}([)";  // it should return FALSE
+        System.out.println(isValid(str));
+    }
+}
