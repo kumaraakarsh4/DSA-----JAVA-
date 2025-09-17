5fe429fe3d3266bcd5ed90ac61853cb1461a2473
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-09-18 02:17:03 +0530

    circular queue question

--- Queues.java
+++ Queues.java
@@ -60,52 +60,132 @@
 
 // Next Adding question
 
+// public class Queues {
+//     public static class Queue{
+//         static int arr[];
+//         static int size;
+//         static int rear;
+//         Queue(int n){
+//             arr = new int[n];
+//             size = n;
+//             rear = -1;
+//         }
+//         public static boolean isEmpty(){
+//             return rear == -1;
+//         }
+//         public static void Add(int data){
+//             if (rear == size-1) {
+//                 System.out.println("Queue is full");
+//                 return;
+                
+//             }
+//             rear =  rear +1;
+//             arr[rear] = data;
+//         }
+//          public String toString() {
+//             StringBuilder sb = new StringBuilder();
+//             sb.append("[");
+//             for (int i = 0; i <= rear; i++) {
+//                 sb.append(arr[i]);
+//                 if (i < rear) sb.append(", ");
+//             }
+//             sb.append("]");
+//             return sb.toString();
+//         }
+//     }
+//     public static void main(String[] args) {
+//         Queue q = new Queue(5);
+//         q.Add(1);
+//         q.Add(2);
+//         q.Add(3);
+//         q.Add(4);
+//         q.Add(5);
+//         System.out.println(q);
+//     }
+// }
+
+//next question of circular Queue 
+
 public class Queues {
+
     public static class Queue{
         static int arr[];
         static int size;
         static int rear;
+        static int front;
         Queue(int n){
             arr = new int[n];
             size = n;
-            rear = -1;
+            rear =  -1;
+            front = -1;
+
         }
         public static boolean isEmpty(){
-            return rear == -1;
+            return rear == -1 && front == -1;
         }
-        public static void Add(int data){
-            if (rear == size-1) {
+        public static boolean isFull(){
+            return  (rear+1)%size == front;
+        }
+        public static void add(int data){
+            if (isFull()) {
                 System.out.println("Queue is full");
-                return;
+                return ;
                 
             }
-            rear =  rear +1;
-            arr[rear] = data;
-        }
-         public String toString() {
-            StringBuilder sb = new StringBuilder();
-            sb.append("[");
-            for (int i = 0; i <= rear; i++) {
-                sb.append(arr[i]);
-                if (i < rear) sb.append(", ");
+            // in this case we add the first element
+            if (front == -1) {
+                front = 0;
+                
             }
-            sb.append("]");
-            return sb.toString();
+            rear = (rear+1)%size;
+            arr[rear] =  data;
+        }
+        public static int remove(){
+            if (isEmpty()) {
+                System.out.println("Queue is empty");
+                return -1;
+                
+            }
+            int result = arr[front];
+            
+            // removing the last element
+            if (rear == front) {
+                rear = front = -1;
+                
+            }else{
+                front = (front+1)%size;
+
+            }
+            return result;
+        }
+        public static int peek(){
+            if (isEmpty()) {
+                System.out.println("queue is full");
+                return -1 ;
+                
+            }
+            return arr[front];
+
         }
     }
     public static void main(String[] args) {
-        Queue q = new Queue(5);
-        q.Add(1);
-        q.Add(2);
-        q.Add(3);
-        q.Add(4);
-        q.Add(5);
-        System.out.println(q);
+        Queue q = new Queue(3);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+      System.out.println(  q.remove());
+        q.add(4);
+       System.out.println( q.remove());
+        q.add(5);
+
+        while (!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
+            
+        }
+
     }
 }
-
-
-
 
 
 
