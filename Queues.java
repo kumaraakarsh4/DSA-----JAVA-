aa4f394f5e8cbe77b5d0cbee249d7c5121ae04f0
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-09-19 01:27:34 +0530

    interleave two half of Queue

--- Queues.java
+++ Queues.java
@@ -404,35 +404,75 @@
 // }
 
 // FIRST NON REPEATING CHARACTER
-import java.util.*;
-public class Queues {
-    public static void nonRepeating(String str){
-        int freq[]= new int[26]; // for storing A-Z character
-        Queue<Character> q = new LinkedList<>();
-        for(int i =0;i<str.length();i++){
-            char ch = str.charAt(i);
-            q.add(ch);
-            freq[ch-'a']++;
-            while (!q.isEmpty()&& freq[q.peek()-'a']>1) {
-                q.remove();
+// import java.util.*;
+// public class Queues {
+//     public static void nonRepeating(String str){
+//         int freq[]= new int[26]; // for storing A-Z character
+//         Queue<Character> q = new LinkedList<>();
+//         for(int i =0;i<str.length();i++){
+//             char ch = str.charAt(i);
+//             q.add(ch);
+//             freq[ch-'a']++;
+//             while (!q.isEmpty()&& freq[q.peek()-'a']>1) {
+//                 q.remove();
                 
-            }
-            if (q.isEmpty()) {
-                System.out.print(-1 +" ");
+//             }
+//             if (q.isEmpty()) {
+//                 System.out.print(-1 +" ");
                 
-            }else{
-                System.out.print(q.peek() + " ");
-            }
-        }
-        System.out.println();
+//             }else{
+//                 System.out.print(q.peek() + " ");
+//             }
+//         }
+//         System.out.println();
 
-    }
-    public static void main(String[] args) {
-        String str = "bbaxcca";
-        nonRepeating(str);
-    }
+//     }
+//     public static void main(String[] args) {
+//         String str = "bbaxcca";
+//         nonRepeating(str);
+//     }
 
     
+// }
+
+// INTERLEAVE 2 HALF OF QUEUE
+
+import java.util.*;
+public class Queues {
+    public static void interLeave(Queue<Integer> q){
+        Queue<Integer> firstHalf = new LinkedList<>();
+        int size = q.size();
+        for(int i=0;i<size/2;i++){
+            firstHalf.add(q.remove());
+        }
+        while (!firstHalf.isEmpty()) {
+            q.add(firstHalf.remove());
+            q.add(q.remove());
+            
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(2);
+        q.add(4);
+        q.add(6);
+        q.add(8);
+        q.add(10);
+        q.add(12);
+        q.add(14);
+        q.add(16);
+       
+        interLeave(q);
+        while (!q.isEmpty()) {
+            System.out.print(q.remove() + " ");
+            
+        }
+        System.out.println();
+       
+        
+
+    }
 }
    
 
