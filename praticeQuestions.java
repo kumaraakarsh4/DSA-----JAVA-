b534d52073a81f68d9d10cffb86b6f5d463556be
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-09-20 01:24:36 +0530

    selection sort question

--- praticeQuestions.java
+++ praticeQuestions.java
@@ -1,30 +1,60 @@
 
 // IN BUBBLE SORT
-public class praticeQuestions {
-    public static void bubble(int arr[]){
-        for(int turn = 0;turn<arr.length-1;turn++){
-            for(int j= 0;j<arr.length-1-turn;j++){
-                if (arr[j] < arr[j+1]) {
-                    int temp = arr[j+1];
-                    arr[j+1] = arr[j];
-                    arr[j] = temp;
+// public class praticeQuestions {
+//     public static void bubble(int arr[]){
+//         for(int turn = 0;turn<arr.length-1;turn++){
+//             for(int j= 0;j<arr.length-1-turn;j++){
+//                 if (arr[j] < arr[j+1]) {
+//                     int temp = arr[j+1];
+//                     arr[j+1] = arr[j];
+//                     arr[j] = temp;
 
                     
-                }
+//                 }
 
+//             }
+//         }
+//     }
+//     public static void printArr(int arr[]){
+//         for(int i=0;i<arr.length;i++){
+//             System.out.print(arr[i]+" ");
+//         }
+//         System.out.println();
+//     }
+//     public static void main(String[] args) {
+//         int arr[] ={3,6,2,1,8,7,4,5,3,1};
+//        bubble(arr);
+//        printArr(arr);
+//     }
+    
+// IN SELECTION SORT
+
+public class praticeQuestions {
+    public static void selection(int arr[]){
+        for(int turn =0;turn<arr.length;turn++){
+            int minPos = turn;
+            for(int j=turn ; j<arr.length;j++){
+                if (arr[minPos]  < arr[j]) {
+                    minPos = j;
+                    
+                }
             }
+            int temp = arr[turn];
+            arr[turn] = arr[minPos];
+            arr[minPos] = temp;
+
         }
     }
     public static void printArr(int arr[]){
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
+        for(int i =0;i<arr.length;i++){
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
+
     public static void main(String[] args) {
         int arr[] ={3,6,2,1,8,7,4,5,3,1};
-       bubble(arr);
-       printArr(arr);
+        selection(arr);
+        printArr(arr);
     }
-    
 }
