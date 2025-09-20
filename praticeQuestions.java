2a2f2f465775ce9a34ec1be7205ce73b9604c346
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-09-21 01:47:30 +0530

    bit manipulation question

--- praticeQuestions.java
+++ praticeQuestions.java
@@ -29,32 +29,51 @@
     
 // IN SELECTION SORT
 
-public class praticeQuestions {
-    public static void selection(int arr[]){
-        for(int turn =0;turn<arr.length;turn++){
-            int minPos = turn;
-            for(int j=turn ; j<arr.length;j++){
-                if (arr[minPos]  < arr[j]) {
-                    minPos = j;
+// public class praticeQuestions {
+//     public static void selection(int arr[]){
+//         for(int turn =0;turn<arr.length;turn++){
+//             int minPos = turn;
+//             for(int j=turn ; j<arr.length;j++){
+//                 if (arr[minPos]  < arr[j]) {
+//                     minPos = j;
                     
-                }
-            }
-            int temp = arr[turn];
-            arr[turn] = arr[minPos];
-            arr[minPos] = temp;
+//                 }
+//             }
+//             int temp = arr[turn];
+//             arr[turn] = arr[minPos];
+//             arr[minPos] = temp;
 
-        }
-    }
-    public static void printArr(int arr[]){
-        for(int i =0;i<arr.length;i++){
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-    }
+//         }
+//     }
+//     public static void printArr(int arr[]){
+//         for(int i =0;i<arr.length;i++){
+//             System.out.print(arr[i] + " ");
+//         }
+//         System.out.println();
+//     }
+
+//     public static void main(String[] args) {
+//         int arr[] ={3,6,2,1,8,7,4,5,3,1};
+//         selection(arr);
+//         printArr(arr);
+//     }
+// }
+  
+// ------------------------------------------------>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// BITMANIPULATION QUESTION PRACTICE
+
+public class praticeQuestions {
 
     public static void main(String[] args) {
-        int arr[] ={3,6,2,1,8,7,4,5,3,1};
-        selection(arr);
-        printArr(arr);
+        int x =3  ;
+        int y =4;
+        System.out.println("Before swap x = " + x + "y = " + y);
+        x= x^y;
+        y=x^y;
+        x=x^y;
+        System.out.println("After swap x =" + x + "y =" + y);
+
     }
 }
+
