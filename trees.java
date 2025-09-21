9363fb0c0c27478025cdc4eae76b926ac5358b76
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-09-22 00:49:49 +0530

    preorder traversal in binary tree

--- trees.java
+++ trees.java
@@ -125,10 +125,76 @@
 
 // FINDING THE MAXIMUM VALUE OF TREE
 
+// import java.util.*;
+// public class trees {
+
+//     public static class Node{
+//         int val;
+//         Node left;
+//         Node right;
+//         public  Node(int val){
+//             this.val = val;
+
+//         }
+//     }
+//     public static int max(Node root){
+//         if (root == null) {
+//             return 0;
+            
+//         }
+//         int a = root.val;
+//         int b= max(root.left);
+//         int c = max(root.right);
+//         return Math.max(a, Math.max(b, c));
+//     }
+//     public static int height(Node root){
+//         if (root == null) {
+//             return 0;
+            
+//         }
+//         if (root.left == null && root.right == null) {
+//             return 0;
+            
+//         }
+//         return 1 + Math.max(height(root.left), height(root.right));
+//     }
+//     public static void main(String[] args) {
+//         Node root = new Node(1);
+//         Node a = new Node(2);
+//         Node b = new Node(3);
+//         root.left = a;
+//         root.right = b;
+//         Node c = new Node(4);
+//         Node d = new Node(5);
+//         a.left = c ;
+//         a.right = d;
+//         Node e = new Node(6);
+//         Node f = new Node(7);
+//         b.left = e;
+//         b.right = f;
+//         System.out.println(max(root));
+//         System.out.println(height(root));
+        
+//     }
+// }
+
+// PRINTING PRE-ORDER TRAVERSAL OF TREE
 import java.util.*;
 public class trees {
 
-    public static class Node{
+    public class preInPost{
+        public static void pip(int n){
+            if (n==0)  return ;
+            System.out.println("pre " + n);
+            pip(n-1);
+            System.out.println("in " + n);
+            pip(n-1);
+            System.out.println("pre " + n);
+                
+            
+        }
+    }
+          public static class Node{
         int val;
         Node left;
         Node right;
@@ -136,30 +202,20 @@ public class trees {
             this.val = val;
 
         }
+       
     }
-    public static int max(Node root){
+    public static void preOrder(Node root){
         if (root == null) {
-            return 0;
+            return;
             
         }
-        int a = root.val;
-        int b= max(root.left);
-        int c = max(root.right);
-        return Math.max(a, Math.max(b, c));
+        System.out.println(root.val);
+        preOrder(root.left);
+        preOrder(root.right);
     }
-    public static int height(Node root){
-        if (root == null) {
-            return 0;
-            
-        }
-        if (root.left == null && root.right == null) {
-            return 0;
-            
-        }
-        return 1 + Math.max(height(root.left), height(root.right));
-    }
-    public static void main(String[] args) {
-        Node root = new Node(1);
+
+public static void main(String[] args) {
+    Node root = new Node(1);
         Node a = new Node(2);
         Node b = new Node(3);
         root.left = a;
@@ -172,8 +228,7 @@ public class trees {
         Node f = new Node(7);
         b.left = e;
         b.right = f;
-        System.out.println(max(root));
-        System.out.println(height(root));
-        
-    }
+        preOrder(root);
+    
+}
 }
