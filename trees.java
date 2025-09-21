88767f397b1a435dadb5033dd131ab5eb943a97d
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-09-22 02:14:41 +0530

    bfs breath first search level order traversal question

--- trees.java
+++ trees.java
@@ -347,21 +347,85 @@
 // }
 
 // PRINTING NTH LEVEL ELEMENT
+// import java.util.*;
+// public class trees {
+
+//     public class preInPost{
+//         public static void pip(int n){
+//             if (n==0)  return ;
+//             System.out.println("pre " + n);
+//             pip(n-1);
+//             System.out.println("in " + n);
+//             pip(n-1);
+//             System.out.println("pre " + n);
+                
+            
+//         }
+//     }
+//           public static class Node{
+//         int val;
+//         Node left;
+//         Node right;
+//         public  Node(int val){
+//             this.val = val;
+
+//         }
+       
+//     }
+//     public static void nthLevel(Node root , int n){
+//       if (root == null){
+//         return;
+//       }
+//       if (n==1){
+//          System.out.print(root.val + " ");
+//          return;
+//       } 
+      
+//       nthLevel(root.left, n-1);
+//       nthLevel(root.right, n-1);
+//     }
+//     public static int height(Node root){
+//         if (root == null) {
+//             return 0;
+            
+//         }
+//         if (root.left == null && root.right == null) {
+//             return 0;
+            
+//         }
+//         return 1 + Math.max(height(root.left), height(root.right));
+//     }
+
+// public static void main(String[] args) {
+//     Node root = new Node(1);
+//         Node a = new Node(2);
+//         Node b = new Node(3);
+//         root.left = a;
+//         root.right = b;
+//         Node c = new Node(4);
+//         Node d = new Node(5);
+//         a.left = c ;
+//         a.right = d;
+//         Node e = new Node(6);
+//         Node f = new Node(7);
+//         b.left = e;
+//         b.right = f;
+//         int level = height(root) + 1;
+//       for(int i=1;i<=level;i++){
+//        nthLevel(root, i);
+//        System.out.println();
+
+//       }
+    
+// }
+// }
+
+// BFS [BREATH FIRST SEARCH] LEVEL ORDER TRAVERSAL
+
 import java.util.*;
 public class trees {
 
-    public class preInPost{
-        public static void pip(int n){
-            if (n==0)  return ;
-            System.out.println("pre " + n);
-            pip(n-1);
-            System.out.println("in " + n);
-            pip(n-1);
-            System.out.println("pre " + n);
-                
-            
-        }
-    }
+  
           public static class Node{
         int val;
         Node left;
@@ -372,19 +436,20 @@ public class trees {
         }
        
     }
-    public static void nthLevel(Node root , int n){
-      if (root == null){
-        return;
-      }
-      if (n==1){
-         System.out.print(root.val + " ");
-         return;
-      } 
-      
-      nthLevel(root.left, n-1);
-      nthLevel(root.right, n-1);
-    }
+    public static void bfs(Node root){
+        Queue<Node> q = new LinkedList<>();
+        if(root!= null) q.add(root);
+        while (q.size()>0) {
+            Node temp = q.peek();
+            if(temp.left != null) q.add(temp.left);
+            if(temp.right != null) q.add(temp.right);
+            System.out.print(temp.val + " ");
+            q.remove();
+            
+        }
 
+    }
+   
 public static void main(String[] args) {
     Node root = new Node(1);
         Node a = new Node(2);
@@ -399,7 +464,8 @@ public static void main(String[] args) {
         Node f = new Node(7);
         b.left = e;
         b.right = f;
-       nthLevel(root, 3);
+        bfs(root);
+       
     
 }
 }
