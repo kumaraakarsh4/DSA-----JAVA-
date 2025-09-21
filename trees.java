17095fab18a3da926c09b1d2233eeaab9ce4dac9
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-09-22 01:04:34 +0530

    postorder traversal

--- trees.java
+++ trees.java
@@ -234,6 +234,62 @@
 // }
 
 // INORDER TRAVERSAL OF BINARY TREE
+// import java.util.*;
+// public class trees {
+
+//     public class preInPost{
+//         public static void pip(int n){
+//             if (n==0)  return ;
+//             System.out.println("pre " + n);
+//             pip(n-1);
+//             System.out.println("in " + n);
+//             pip(n-1);
+//             System.out.println("pre " + n);
+                
+            
+//         }
+//     }
+//           public static class Node{
+//         int val;
+//         Node left;
+//         Node right;
+//         public  Node(int val){
+//             this.val = val;
+
+//         }
+       
+//     }
+//     public static void inOrder(Node root){
+//         if (root == null) {
+//             return;
+            
+//         }
+       
+//         inOrder(root.left);
+//          System.out.println(root.val);
+//         inOrder(root.right);
+//     }
+
+// public static void main(String[] args) {
+//     Node root = new Node(1);
+//         Node a = new Node(2);
+//         Node b = new Node(3);
+//         root.left = a;
+//         root.right = b;
+//         Node c = new Node(4);
+//         Node d = new Node(5);
+//         a.left = c ;
+//         a.right = d;
+//         Node e = new Node(6);
+//         Node f = new Node(7);
+//         b.left = e;
+//         b.right = f;
+//         inOrder(root);
+    
+// }
+// }
+
+// POST ORDER TRAVERSAL IN TREE
 import java.util.*;
 public class trees {
 
@@ -259,15 +315,16 @@ public class trees {
         }
        
     }
-    public static void inOrder(Node root){
+    public static void postOrder(Node root){
         if (root == null) {
             return;
             
         }
        
-        inOrder(root.left);
+        postOrder(root.left);
+        
+        postOrder(root.right);
          System.out.println(root.val);
-        inOrder(root.right);
     }
 
 public static void main(String[] args) {
@@ -284,7 +341,7 @@ public static void main(String[] args) {
         Node f = new Node(7);
         b.left = e;
         b.right = f;
-        inOrder(root);
+        postOrder(root);
     
 }
 }
