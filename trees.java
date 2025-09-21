22d5ca92331074e0c0ed5596904914b0e5654fcb
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-09-21 18:24:14 +0530

    finding Height and Max value of tree

--- trees.java
+++ trees.java
@@ -77,6 +77,54 @@
 // FIND SIZE OF THE BINARY TREE
 // ALSO FINDING THE SUM OF NODES OF THE TREE
 
+// import java.util.*;
+// public class trees {
+
+//     public static class Node{
+//         int val;
+//         Node left;
+//         Node right;
+//         public  Node(int val){
+//             this.val = val;
+
+//         }
+//     }
+//     public static int size(Node root){
+//         if (root == null) {
+//             return 0; 
+            
+//         }
+//         return 1 + size(root.left) + size(root.right) ;
+//     }
+//     public static int sum(Node root){
+//         if (root == null) {
+//             return 0;
+            
+//         }
+//         return root.val + sum(root.left) + sum(root.right);
+//     }
+
+//     public static void main(String[] args) {
+//         Node root = new Node(1);
+//         Node a = new Node(2);
+//         Node b = new Node(3);
+//         root.left = a;
+//         root.right = b;
+//         Node c = new Node(4);
+//         Node d = new Node(5);
+//         a.left = c ;
+//         a.right = d;
+//         Node e = new Node(6);
+//         Node f = new Node(7);
+//         b.left = e;
+//         b.right = f;
+//         System.out.println(size(root));
+//         System.out.println(sum(root));
+//     }
+// }
+
+// FINDING THE MAXIMUM VALUE OF TREE
+
 import java.util.*;
 public class trees {
 
@@ -89,21 +137,27 @@ public class trees {
 
         }
     }
-    public static int size(Node root){
-        if (root == null) {
-            return 0; 
-            
-        }
-        return 1 + size(root.left) + size(root.right) ;
-    }
-    public static int sum(Node root){
+    public static int max(Node root){
         if (root == null) {
             return 0;
             
         }
-        return root.val + sum(root.left) + sum(root.right);
+        int a = root.val;
+        int b= max(root.left);
+        int c = max(root.right);
+        return Math.max(a, Math.max(b, c));
     }
-
+    public static int height(Node root){
+        if (root == null) {
+            return 0;
+            
+        }
+        if (root.left == null && root.right == null) {
+            return 0;
+            
+        }
+        return 1 + Math.max(height(root.left), height(root.right));
+    }
     public static void main(String[] args) {
         Node root = new Node(1);
         Node a = new Node(2);
@@ -118,7 +172,8 @@ public class trees {
         Node f = new Node(7);
         b.left = e;
         b.right = f;
-        System.out.println(size(root));
-        System.out.println(sum(root));
+        System.out.println(max(root));
+        System.out.println(height(root));
+        
     }
 }
