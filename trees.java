280039f85b63958f30d5d5f49312c8f985d7717c
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-09-21 18:04:27 +0530

    finding Size and Sum of tree

--- trees.java
+++ trees.java
@@ -22,46 +22,103 @@
  
 // IMPLEMENTATION OF TREE OR DISPLAY OF TREE
 
+// import java.util.*;
+// public class trees {
+//     public static class Node{
+//         int val;
+//         Node left = null;
+//         Node right = null;
+//         public Node(int val){
+//             this.val = val;
+
+//         }
+//     }
+//     public static void display(Node root){
+//         if (root == null) {
+//             return;
+            
+//         }
+//         System.out.print(root.val + "-> " );
+//         if(root.left != null){
+//              System.out.print(root.left.val + "  ");
+//         }else{
+//             System.out.print("null" + " ");
+//         }
+//         if(root.right != null){
+//             System.out.print(root.right.val + " ");
+//         } else{
+//             System.out.print("null" + " ");
+//         }
+//         System.out.println();
+//         display(root.left);
+//         display(root.right);
+
+//     }
+
+//     public static void main(String[] args) {
+//         Node root = new Node(2);
+//         Node a = new Node(4);
+//         Node b = new Node(10);
+//         root.left = a;
+//         root.right = b;
+//         Node c = new Node(6);
+//         Node d = new Node(5);
+//         a.left = c;
+//         a.right = d;
+//         Node e = new Node(11);
+//         b.right = e;
+//         display(root);
+
+
+        
+//     }
+// }
+
+// FIND SIZE OF THE BINARY TREE
+// ALSO FINDING THE SUM OF NODES OF THE TREE
+
 import java.util.*;
 public class trees {
+
     public static class Node{
         int val;
-        Node left = null;
-        Node right = null;
-        public Node(int val){
+        Node left;
+        Node right;
+        public  Node(int val){
             this.val = val;
 
         }
     }
-    public static void display(Node root){
+    public static int size(Node root){
         if (root == null) {
-            return;
+            return 0; 
             
         }
-        System.out.print(root.val + "-> " );
-        if(root.left != null) System.out.print(root.left.val + "  ");
-        if(root.right != null) System.out.print(root.right.val + " ");
-        System.out.println();
-        display(root.left);
-        display(root.right);
-
+        return 1 + size(root.left) + size(root.right) ;
+    }
+    public static int sum(Node root){
+        if (root == null) {
+            return 0;
+            
+        }
+        return root.val + sum(root.left) + sum(root.right);
     }
 
     public static void main(String[] args) {
-        Node root = new Node(2);
-        Node a = new Node(4);
-        Node b = new Node(10);
+        Node root = new Node(1);
+        Node a = new Node(2);
+        Node b = new Node(3);
         root.left = a;
         root.right = b;
-        Node c = new Node(6);
+        Node c = new Node(4);
         Node d = new Node(5);
-        a.left = c;
+        a.left = c ;
         a.right = d;
-        Node e = new Node(11);
-        b.right = e;
-        display(root);
-
-
-        
+        Node e = new Node(6);
+        Node f = new Node(7);
+        b.left = e;
+        b.right = f;
+        System.out.println(size(root));
+        System.out.println(sum(root));
     }
 }
