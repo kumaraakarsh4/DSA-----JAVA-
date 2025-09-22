2006415abdfdab89f402b7c34ec8c103f068de84
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-09-23 02:03:31 +0530

    question of balance tree

--- trees.java
+++ trees.java
@@ -472,6 +472,54 @@
 
 // BFS NEXT QUESTION
 
+// import java.util.*;
+// public class trees {
+
+  
+//           public static class Node{
+//         int val;
+//         Node left;
+//         Node right;
+//         public  Node(int val){
+//             this.val = val;
+
+//         }
+       
+//     }
+//     public static void bfs(Node root){
+//         Queue<Node> q = new LinkedList<>();
+//         if(root!= null) q.add(root);
+//         while (q.size()>0) {
+//             Node temp = q.peek();
+//             if(temp.left != null) q.add(temp.left);
+//             if(temp.right != null) q.add(temp.right);
+//             System.out.print(temp.val + " ");
+//             q.remove();
+            
+//         }
+
+//     }
+   
+// public static void main(String[] args) {
+//     Node root = new Node(1);
+//         Node a = new Node(2);
+//         Node b = new Node(3);
+//         root.left = a;
+//         root.right = b;
+//         Node c = new Node(4);
+//         Node d = new Node(5);
+//         a.left = c ;
+//         a.right = d;
+//         Node e = new Node(6);
+//         Node f = new Node(7);
+//         b.left = e;
+//         b.right = f;
+//         bfs(root);
+       
+    
+// }
+// }
+
 import java.util.*;
 public class trees {
 
@@ -486,36 +534,32 @@ public class trees {
         }
        
     }
-    public static void bfs(Node root){
-        Queue<Node> q = new LinkedList<>();
-        if(root!= null) q.add(root);
-        while (q.size()>0) {
-            Node temp = q.peek();
-            if(temp.left != null) q.add(temp.left);
-            if(temp.right != null) q.add(temp.right);
-            System.out.print(temp.val + " ");
-            q.remove();
-            
-        }
-
+    public static int height(Node root){
+        if(root == null || root.left == null && root.right==null) return 0;
+        return 1 + Math.max(height(root.left), height(root.right));
     }
-   
-public static void main(String[] args) {
-    Node root = new Node(1);
-        Node a = new Node(2);
-        Node b = new Node(3);
+    public static boolean balanceTree(Node root){
+        if(root == null) return true;
+        int lh = height(root.left);
+        int rh = height(root.right);
+        int d = lh - rh;
+        if(d<0) d = -d;
+        if(d<1) return false;
+        return (balanceTree(root.left) && balanceTree(root.right));
+    }
+
+    public static void main(String[] args) {
+    Node root = new Node(3);
+        Node a = new Node(9);
+        Node b = new Node(20);
         root.left = a;
         root.right = b;
-        Node c = new Node(4);
-        Node d = new Node(5);
-        a.left = c ;
-        a.right = d;
-        Node e = new Node(6);
+       
+        Node e = new Node(15);
         Node f = new Node(7);
         b.left = e;
         b.right = f;
-        bfs(root);
-       
-    
+        height(root);
+       System.out.println(balanceTree(root));
 }
 }
