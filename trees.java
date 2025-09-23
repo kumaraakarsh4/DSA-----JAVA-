4c5787b7c3ce84cc40a59d46d15fa7fbc8ab2f13
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-09-24 01:36:48 +0530

    praticing tree question

--- trees.java
+++ trees.java
@@ -619,6 +619,64 @@
 
 // ZIG ZAG PRINTING OF TREE
 
+// public class trees {
+
+//     public static class Node{
+//         int val;
+//         Node left;
+//         Node right;
+//         public  Node(int val){
+//             this.val = val;
+
+// }
+//     }
+//     public static void nthlevel(Node root , int n){
+//         if (root == null) {
+//             return;
+            
+//         }
+//         if (n==1) {
+//             System.out.print(root.val + " ");
+            
+//         }
+//         nthlevel(root.left, n-1);
+//         nthlevel(root.right, n-1);
+//     }
+//     public static int height(Node root){
+//         if (root == null) {
+//             return 0;
+            
+//         }
+//         if (root.left==null && root.right==null) {
+//             return 0;
+            
+//         }
+//         return 1+ Math.max(height(root.left), height(root.right));
+//     }
+//     public static void main(String[] args) {
+//         Node root = new Node(1);
+//         Node a = new Node(2);
+//         Node b = new Node(3);
+//         root.left = a;
+//         root.right = b;
+//         Node c = new Node(4);
+//         Node d = new Node(5);
+//         a.left = c ;
+//         a.right = d;
+//         Node e = new Node(6);
+//         Node f = new Node(7);
+//         b.left = e;
+//         b.right = f;
+//         int level = height(root)+1;
+//         for(int i=1;i<=level;i++){
+//             nthlevel(root, i);
+//             System.out.println();
+//         }
+        
+//     }
+// }
+
+import java.util.*;
 public class trees {
 
     public static class Node{
@@ -628,33 +686,24 @@ public class trees {
         public  Node(int val){
             this.val = val;
 
-}
-    }
-    public static void nthlevel(Node root , int n){
-        if (root == null) {
-            return;
-            
         }
-        if (n==1) {
-            System.out.print(root.val + " ");
-            
-        }
-        nthlevel(root.left, n-1);
-        nthlevel(root.right, n-1);
     }
-    public static int height(Node root){
+    public static int size(Node root){
         if (root == null) {
             return 0;
             
         }
-        if (root.left==null && root.right==null) {
+        return 1 + size(root.left) +  size(root.right);
+    }
+    public static int sum(Node root){
+        if (root == null) {
             return 0;
             
         }
-        return 1+ Math.max(height(root.left), height(root.right));
+        return root.val +  sum(root.left) + sum(root.right);
     }
     public static void main(String[] args) {
-        Node root = new Node(1);
+         Node root = new Node(1);
         Node a = new Node(2);
         Node b = new Node(3);
         root.left = a;
@@ -667,11 +716,8 @@ public class trees {
         Node f = new Node(7);
         b.left = e;
         b.right = f;
-        int level = height(root)+1;
-        for(int i=1;i<=level;i++){
-            nthlevel(root, i);
-            System.out.println();
-        }
+        System.out.println(size(root));
+        System.out.println(sum(root));
         
     }
 }
