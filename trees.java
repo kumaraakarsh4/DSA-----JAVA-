f7f558e0864186084d02c437ea935326e9cb13a6
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-09-25 01:50:45 +0530

    question of tree lowest common ancestor

--- trees.java
+++ trees.java
@@ -844,6 +844,45 @@
 // }
 // }
 
+// import java.util.*;
+// public class trees {
+//     public static class Node{
+//         int val;
+//         Node left;
+//         Node right;
+//         public Node(int val){
+//             this.val = val;
+//         }
+//     }
+//     public static void kLevel(Node root , int level , int k){
+//         if (root == null) {
+//             return ;
+            
+//         }
+//         if (k==level) {
+//             System.out.print(root.val + " ");
+//             return;
+
+            
+//         }
+//         kLevel(root.left, level+1, k);
+//         kLevel(root.right, level+1, k);
+//     }
+// public static void main(String[] args) {
+//  Node root = new Node(1);
+//  root.left = new Node(2);
+//  root.right = new Node(3);
+//  root.left.left = new Node(4);
+//  root.left.right = new Node(5);
+//  root.right.left = new Node(6);
+//  root.right.right = new Node(7);
+//  int k = 3;
+//  kLevel(root, 1, k);   
+// }
+// }
+
+// LOWEST COMMON ANCESTOR
+
 import java.util.*;
 public class trees {
     public static class Node{
@@ -854,29 +893,50 @@ public class trees {
             this.val = val;
         }
     }
-    public static void kLevel(Node root , int level , int k){
-        if (root == null) {
-            return ;
-            
+    public static Node lca(Node root , int n1 ,int n2){
+        ArrayList<Node> path1 = new ArrayList<>();
+        ArrayList<Node> path2 = new ArrayList<>();
+        getpath(root ,n1 , path1);
+        getpath(root , n2 , path2);
+        int i=0;
+        for(; i<path1.size() && i<path2.size();i++){
+            if (path1.get(i)!= path2.get(i)) {
+                break;
+                
+            }
         }
-        if (k==level) {
-            System.out.print(root.val + " ");
-            return;
-
-            
-        }
-        kLevel(root.left, level+1, k);
-        kLevel(root.right, level+1, k);
+        Node lca = (path1.get(i-1));
+        return lca;
     }
-public static void main(String[] args) {
- Node root = new Node(1);
- root.left = new Node(2);
- root.right = new Node(3);
- root.left.left = new Node(4);
- root.left.right = new Node(5);
- root.right.left = new Node(6);
- root.right.right = new Node(7);
- int k = 3;
- kLevel(root, 1, k);   
-}
+    public static boolean getpath(Node root , int n , ArrayList<Node> path){
+        if (root==null) {
+            return false;
+            
+        }
+        path.add(root);
+        if (root.val == n) {
+            return true;
+            
+        }
+       boolean foundleft =getpath(root.left, n, path);
+       boolean foundright = getpath(root.right, n, path);
+       if (foundleft || foundright) {
+        return true;
+        
+       }
+       path.remove(path.size()-1);
+       return false;
+    }
+
+    public static void main(String[] args) {
+     Node root = new Node(1);
+     root.left = new Node(2);
+     root.right = new Node(3);
+     root.left.left = new Node(4);
+     root.left.right  =  new Node(5);
+     root.right.left = new Node(6);
+     root.right.right = new Node(7);  
+     int n1 = 4 , n2 = 7;
+     System.out.println(lca(root, n1, n2).val);
+    }
 }
