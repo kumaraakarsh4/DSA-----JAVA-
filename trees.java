8c4b34ee21af956d13902e3b5dabd556c9cd9d9c
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-09-26 02:24:56 +0530

    printing the paths from root to leaf node

--- trees.java
+++ trees.java
@@ -1160,36 +1160,36 @@ public class trees {
         System.out.print(root.val + " ");
         inOrder(root.right);
     }
-    public static void printInRange(Node root , int k1 ,  int k2){
-        if (root == null) {
-            return;
-            
-        }
-        if (root.val >= k1 && root.val <= k2) {
-            printInRange(root.left, k1, k2);
-            System.out.print(root.val + " ") ;
-            printInRange(root.right, k1, k2);
-            
-        }
-        else if (root.val < k1) {
-            printInRange(root.right, k1, k2);
-            
-        }
-        else{
-            printInRange(root.left, k1, k2);
-        }
+  public static void printLeaf(Node root , ArrayList<Integer> path){
+    if (root == null) {
+        return;
+        
     }
-
+    path.add(root.val);
+    if (root.left == null && root.right == null) {
+        printPath(path);
+        
+    }
+    printLeaf(root.left, path);
+    printLeaf(root.right, path);
+    path.remove(path.size()-1);
+  }
+  public static void printPath(ArrayList<Integer> path){
+    for(int i=0;i<path.size();i++){
+        System.out.print(path.get(i) + " ");
+    }
+    System.out.println("Null");
+  }
     
 public static void main(String[] args) {
-     int values[] = {1,3,6,8,9,5,4,10,2,7};
+     int values[] = {8,5,3,6,10,11,14};
      Node root = null;
        for(int i=0;i<values.length;i++){
            root =  insert(root, values[i]);
         }
         inOrder(root);
         System.out.println();
-      printInRange(root, 3, 7);
+     printLeaf(root, new ArrayList<>());
     }
 }
    
