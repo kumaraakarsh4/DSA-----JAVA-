4e574d4ce87d7e893fe1338afd9d1f5c5d8362d9
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-09-27 00:24:35 +0530

    sorted bst  array

--- trees.java
+++ trees.java
@@ -1156,8 +1156,9 @@ public class trees {
             return;
             
         }
+         System.out.print(root.val + " ");
         inOrder(root.left);
-        System.out.print(root.val + " ");
+       
         inOrder(root.right);
     }
  public static boolean validate(Node root , Node min , Node max){
@@ -1177,21 +1178,23 @@ public class trees {
     validate(root.right, root, max);
 
  }
+ public static Node bst(int arr[] ,int st ,int end){
+    if (st > end) {
+        return null;
+        
+    }
+    int mid = (st+end)/2;
+    Node root = new Node(arr[mid]);
+    root.left = bst(arr, st, mid-1);
+    root.right = bst(arr, mid+1, end);
+    return root;
+
+ }
     
 public static void main(String[] args) {
-     int values[] = {1,1,1};
-     Node root = null;
-       for(int i=0;i<values.length;i++){
-           root =  insert(root, values[i]);
-        }
-        inOrder(root);
-        System.out.println();
-   if (validate(root, null, null)) {
-    System.out.println("valid");
-    
-   }else{
-    System.out.println("Not valid");
-   }
+     int arr[] = {3,5,6,8,10,11,12};
+    Node root = bst(arr, 0, arr.length-1);
+   inOrder(root);
     }
 }
    
