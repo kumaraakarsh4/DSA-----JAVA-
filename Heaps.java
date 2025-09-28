cc13beb969e3f3d23c4771cd68936fa8099868f5
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-09-29 01:31:53 +0530

    Heap swapping index swapping question

--- Heaps.java
+++ Heaps.java
@@ -21,34 +21,75 @@
 import java.util.*;
 public class Heaps {
 
-    static class heap{
+    static class Heap{
         ArrayList<Integer> arr = new ArrayList<>();
         public void add(int data){
             arr.add(data);
             int x = arr.size()-1;
             int par = (x-1)/2;
-            while (arr.get(x) < arr.get(par)) {
+            while (x> 0  && arr.get(x) < arr.get(par)) {
                 int temp = arr.get(x);
                 arr.set(x,arr.get(par));
-                arr.set(temp , par);
+                arr.set(par ,temp);
+                x = par;
+                par = (x-1)/2;
                 
             }
         }
         public int peek(){
             return arr.get(0);
         }
-    }
-    public static void main(String[] args) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        pq.add(1);
-        pq.add(5);
-        pq.add(7);
-        pq.add(2);
-        pq.add(0);
-        while (!pq.isEmpty()) {
-            System.out.println(pq.peek());
-            pq.remove();
-            
+        public int remove(){
+            if(arr.size()==0) return -1;
+            int data = arr.get(0); //
+            arr.set(0, arr.get(arr.size()-1));
+          
+            // for removing index
+            arr.remove(arr.size()-1);
+            heapify(0);
+            return data;
+
+
+
         }
+        public void heapify(int i){
+            int left = 2*i+1;
+            int right = 2*i+2;
+            int mindix = i;
+            if (left < arr.size() && arr.get(left )  <  arr.get(mindix)  ) {
+                mindix = left;
+                
+            }
+            if (right < arr.size() &&  arr.get(right) < arr.get(mindix)  ) {
+                mindix = right;
+                
+            }
+            if (mindix != i) {
+                int temp = arr.get(i);
+                arr.set(i, arr.get(mindix));
+                arr.set(mindix, temp);
+                heapify(mindix);
+                
+            }
+        }
+        public boolean isEmpty(){
+            return arr.size() == 0;
+        }
+    }
+  
+    public static void main(String[] args) {
+       Heap h = new Heap();
+       h.add(3);
+       h.add(4);
+       h.add(1);
+       h.add(5);
+       while (!h.isEmpty()) {
+        System.out.println(h.peek());
+        h.remove();
+        
+       }
+    
+       
+      
     }
 }
