1bb21f2a49409ba2370b94bcddf107ab19a5e8d9
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-10-02 23:05:17 +0530

    heap next question

--- Heaps.java
+++ Heaps.java
@@ -287,6 +287,44 @@
 //     }
 // }
 
+// import java.util.*;
+// public class Heaps {
+//     static class Row implements Comparable<Row>{
+//         int Soldier;
+//         int indx;
+//         public Row(int Soldier , int indx){
+//             this.Soldier = Soldier;
+//             this.indx = indx;
+//         }
+//         @Override 
+//         public int compareTo(Row r2){
+//             if (this.Soldier== r2.Soldier) {
+//                 return this.indx - r2.indx;
+                
+//             }else{
+//                 return this.Soldier - r2.Soldier;
+//             }
+//         }
+//     }
+
+//     public static void main(String[] args) {
+//         int army [][] = {{1,0,0,0},
+//         {1,1,1,1},{1,0,0,0},{1,0,0,0}};
+//         int k =2;
+//         PriorityQueue<Row> pq = new PriorityQueue<>();
+//         for(int i=0;i<army.length;i++){
+//             int count =0;
+//             for(int j=0;j<army[0].length;j++){
+//                 count += army[i][j] ==1 ? 1:0;
+//             }
+//             pq.add(new Row(count, i));
+//         }
+//         for(int i=0;i<k;i++){
+//             System.out.println("R" + pq.remove().indx);
+//         }
+//     }
+// }
+
 import java.util.*;
 public class Heaps {
     static class Row implements Comparable<Row>{
